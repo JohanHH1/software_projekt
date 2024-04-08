c632d4332ff9563b443f7c70a2e565bc73767efd
JohanHH1 <johan.holm.hansen@gmail.com>
2024-04-08 14:48:47 +0200

    Added Login and Project class

---
+++ javaFX_project/src/test/java/example/cucumber/Login.java
@@ -0,0 +1,8 @@
+package example.cucumber;
+
+import java.util.ArrayList;
+
+public class Login {
+    private String initials;
+    public ArrayList<String> employees;
+}
---
+++ javaFX_project/src/test/java/example/cucumber/Project.java
@@ -0,0 +1,6 @@
+package example.cucumber;
+
+public class Project {
+    private int projectNumber;
+    private String projectName;
+}
